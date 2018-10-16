bd705f6c74e7ba2ae153a13e8f0e548c8e2ce44b
Robert Rice <rrice1291@gmail.com>
2018-10-15 21:25:51 -0500

    added xhr request via jquery stuff

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,12 @@
 // Filter fish that are "on sale"
 
 // Add fish to "Basket"
+
+//Load fish
+$.get('../db/fishes.json')
+.done(()=> {
+    console.log(data);
+})
+.fail((error) =>{
+    console.error({error});
+});
